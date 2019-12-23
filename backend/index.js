f6ac12368a6d2137c66b72b7c08c60f26c939ac5
unknown <p0nta_@hotmail.com>
2019-12-23 09:43:07 +0100

    Backend setup, packages done, simple server file

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,7 @@
-console.log('test');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express()
+
+app.use(bodyParser.json());
+app.get('/', (req, res) => res.send('Welcome To Hundpassnings server'));
